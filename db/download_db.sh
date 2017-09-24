83c55683d6fbb68beb2358ac5126ecaf925837e1
Mariano Rajoy <1octubrereferendum@gmail.com>
2017-09-24 21:43:40 +0000

    Free Catalonia: Update db files with last data

--- db/download_db.sh
+++ db/download_db.sh
@@ -4,6 +4,18 @@ db_url_prefix=https://onvotar.garantiespelreferendum.com/db
 hex_values=`printf "%02x " {0..255}`
 parallel_downloads=10
 
+function check_md5() {
+  local file=$1
+  local file_url=${db_url_prefix}/${file}
+  echo -n "Checking if file ${file} needs to be updated ... "
+  origin_md5=`curl -s ${file_url} -o- | md5sum | awk '{print $1}'`
+  dest_md5=`md5sum ${file} | awk '{print $1}'`
+  if [ ${origin_md5} == ${dest_md5} ]; then
+    return 0
+  fi
+  return 1
+}
+
 function download_db() {
   local file=$1
   local file_url=${db_url_prefix}/${file}
@@ -27,7 +39,12 @@ for folder in ${hex_values}; do
     elif grep "Connection timed out" ${folder}/${file}.db >/dev/null; then
       rm -f ${folder}/${file}.db
       download_db ${folder}/${file}.db
+    elif ! check_md5 ${folder}/${file}.db; then
+      echo "Updating file ${folder}/${file}.db ..."
+      rm -f ${folder}/${file}.db
+      download_db ${folder}/${file}.db
     fi
+    echo
   done
   )
 done
